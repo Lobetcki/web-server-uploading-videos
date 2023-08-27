a72b270370eae4bd69901a56b11b3c5d228a924b
Lobetcki <Lobetcki@gmail.com>
2023-08-27 09:59:49 +0300

    Initial commit

---
+++ src/main/java/com/example/webserveruploadingvideos/WebServerUploadingVideosApplication.java
@@ -0,0 +1,13 @@
+package com.example.webserveruploadingvideos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServerUploadingVideosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebServerUploadingVideosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/webserveruploadingvideos/WebServerUploadingVideosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webserveruploadingvideos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebServerUploadingVideosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
